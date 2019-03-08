c5223a0d176a2e73e682f5b3787208c221bba481
kevinbece5 <kevinbece5@gmail.com>
2019-03-07 16:55:58 -0800

    proxy server up

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
+const newrelic = require('newrelic');
 const express = require('express');
-//const bodyParser = require('body-parser');
-
+const bodyParser = require('body-parser');
+const proxy = require('http-proxy-middleware')
 const app = express();
 const port = 3000;
+const cors = require('cors');
 
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({extended: true}));
-app.use(function(req, res, next) {
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-app.use(express.static(__dirname + '/public'));
+
+
+app.use(
+  '/api/reviews',
+  proxy({
+    target: 'http://localhost:3004',
+    changeOrigin: true
+  })
+);
+app.use('/', express.static(__dirname + '/public'));
 
 app.listen(port, () => {
   console.log(`I'm serving from http://localhost:${port}`);
